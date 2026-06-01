ce2418fbdba15b711c50881b6f0e4eac9f103223
+ <+>
2026-06-01 14:13:51 -0400

    initial addition for feature 1

---
+++ script.js
@@ -0,0 +1,30 @@
+const imageMenu = document.getElementById("image-menu-container");
+const IMAGE_BASE_URL = "http://localhost:3000/";
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:3000/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      data.forEach(photo => {
+        // renderImages(photo);
+    })
+    //   data.forEach(renderImages);
+    });
+
+  function renderImages(photo) {
+    console.log(photo, "photo");
+
+    let linkElement = document.createElement("a");
+    linkElement.href = "#";
+
+    const imgElement = document.createElement("img");
+
+    imgElement.src = photo.src
+    imgElement.alt = photo.title 
+    imgElement.className = "image-menu";
+
+    linkElement.appendChild(imgElement);
+    imageMenu.appendChild(linkElement);
+  }
+});
