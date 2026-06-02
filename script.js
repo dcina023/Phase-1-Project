362037fef2aaadfe42536a17e38d0b909425a4e7
+ <+>
2026-06-02 09:53:15 -0400

    added event listener to imgElement

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+const imageMenu = document.querySelector(".image-menu"); 
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("http://localhost:3000/posts")
+    .then((res) => {
+      if (!res.ok) throw new Error("Backend server error");
+      return res.json();
+    })
+    .then((data) => {
+      console.log("Data received successfully:", data);
+      data.forEach((photo) => {
+        renderImages(photo);
+      });
+    })
+    .catch((err) => console.error("Fetch failed:", err));
+
+  function renderImages(photo) {
+    const linkElement = document.createElement("a");
+    linkElement.href = "#";
+
+    const imgElement = document.createElement("img");
+    
+    
+    imgElement.src = photo.src; 
+    
+
+    imgElement.alt = photo.title; 
+    imgElement.className = "grid-photo"; 
+
+    imgElement.addEventListener("click", (e) => {
+        console.log("clicked")
+    })
+
+    linkElement.appendChild(imgElement);
+    imageMenu.appendChild(linkElement);
+  }
+
+  
+});
+
