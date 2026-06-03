13854027928f666070ff29a3db5125f9baf33589
dcina023 <dpaigecina@gmail.com>
2026-06-03 10:46:49 -0400

    Update script.js
    Co-authored-by: Kyle Fadigan <kyle.fadigan@powerhrg.com>

--- script.js
+++ script.js
@@ -95,7 +95,6 @@ document.addEventListener("DOMContentLoaded", () => {
        if (outputDiv) {
          detailsSection.appendChild(outputDiv);
        }
-       
      });
 
      linkElement.appendChild(imgElement);
